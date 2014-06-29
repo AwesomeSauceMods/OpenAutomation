d99926de63d577854f50373f00c5a1273a3646dc
gjgfuj <gjgfujb@gmail.com>
2014-06-29 10:59:18 +1000

    Updated to OC api 1.3.

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/common/OpenAutomation.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/common/OpenAutomation.scala
@@ -108,11 +108,11 @@ object OpenAutomation extends TAwesomeSauceMod {
     } else {*/
 
     ItemUtil.addRecipe(this, new ShapedOreRecipe(new ItemStack(itemIO), "xyx", "abc", "xyx",
-      Character.valueOf('x'), "ingotAwesomeite", Character.valueOf('y'), Items.PrintedCircuitBoard,
+      Character.valueOf('x'), "ingotAwesomeite", Character.valueOf('y'), Items.get("printedCircuitBoard").item(),
       Character.valueOf('a'), inputCode, Character.valueOf('b'), itemCode, Character.valueOf('c'), outputCode))
-    ItemUtil.addRecipe(this, new ShapelessOreRecipe(new ItemStack(codeBundle), Items.MicrochipTier1, "blockAwesomeite"))
-    ItemUtil.addRecipe(this, new ShapelessOreRecipe(new ItemStack(codeBundle), Items.MicroChipTier2, "ingotAwesomeite", "ingotAwesomeite", "ingotAwesomeite"))
-    ItemUtil.addRecipe(this, new ShapelessOreRecipe(new ItemStack(codeBundle), Items.MicroChipTier3, "nuggetAwesomeite"))
+    ItemUtil.addRecipe(this, new ShapelessOreRecipe(new ItemStack(codeBundle), Items.get("microchip1").item(), "blockAwesomeite"))
+    ItemUtil.addRecipe(this, new ShapelessOreRecipe(new ItemStack(codeBundle), Items.get("microchip2").item(), "ingotAwesomeite", "ingotAwesomeite", "ingotAwesomeite"))
+    ItemUtil.addRecipe(this, new ShapelessOreRecipe(new ItemStack(codeBundle), Items.get("microchip3").item(), "nuggetAwesomeite"))
     //}
   }
   def postInit() = {}

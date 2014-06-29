585d2f501694a41d175dfe3b56630afd2cc6c935
gjgfuj <gjgfujb@gmail.com>
2014-06-29 10:58:39 +1000

    Did a clean up, and made the ore crusher work with oredict support.

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/addons/tile/TileEntityPressureCrusher.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/addons/tile/TileEntityPressureCrusher.scala
@@ -1,55 +1,60 @@
 package com.awesomesauce.minecraft.forge.openautomation.addons.tile
 
 import com.awesomesauce.minecraft.forge.core.components.AwesomeSauceComponents
-import com.awesomesauce.minecraft.forge.openautomation.api.ItemStorage
+import com.awesomesauce.minecraft.forge.core.lib.util.InventoryWrapper
+import com.awesomesauce.minecraft.forge.openautomation.api.{ItemDestination, ItemStorage}
 import com.awesomesauce.minecraft.forge.openautomation.api.tools.AddressPastable
 import li.cil.oc.api.Network
-import li.cil.oc.api.network.Arguments
-import li.cil.oc.api.network.Callback
-import li.cil.oc.api.network.Context
-import li.cil.oc.api.network.Visibility
+import li.cil.oc.api.network.{Arguments, Callback, Context, Visibility}
 import li.cil.oc.api.prefab.TileEntityEnvironment
 import net.minecraft.inventory.IInventory
 import net.minecraft.item.ItemStack
 import net.minecraftforge.oredict.OreDictionary
-import com.awesomesauce.minecraft.forge.core.lib.util.InventoryWrapper
-import com.awesomesauce.minecraft.forge.openautomation.api.ItemDestination
-import com.awesomesauce.minecraft.forge.openautomation.addons.OpenAutomationAddons
 
 class TileEntityPressureCrusher extends TileEntityEnvironment with AddressPastable with ItemStorage with IInventory {
   val node_ = Network.newNode(this, Visibility.Network).withComponent("pressurecrusher").withConnector(1000).create()
   node = node_
   val recipes = AwesomeSauceComponents.grinderRecipes
   var address: String = "xxx"
+
   def pasteAddress(add: String) = address = add
+
   var crushingSlot: ItemStack = null
   var crushedSlot: ItemStack = null
+
   @Callback
   def setAddress(context: Context, arguments: Arguments): Array[AnyRef] = {
     address = arguments.checkString(0)
     Array(true.asInstanceOf[java.lang.Boolean])
   }
+
   @Callback
   def getAddress(context: Context, arguments: Arguments): Array[AnyRef] = {
     Array(address)
   }
+
   @Callback
   def crush(context: Context, arguments: Arguments): Array[AnyRef] = {
     for (oid <- OreDictionary.getOreIDs(crushingSlot)) {
-      val ore = OreDictionary.getOreName()
-      context.pause(5)
-      if (node_.tryChangeBuffer(-100)) {
-        if (crushedSlot == null)
-          crushedSlot = recipes(ore).copy()
-        else if (crushedSlot.getItem() == recipes(ore).getItem())
-          crushedSlot.stackSize += 2
-        else
-          return Array(null, "Target stack full")
-        return Array(true.asInstanceOf[java.lang.Boolean])
+      val ore = OreDictionary.getOreName(oid)
+      if (recipes(ore) != null) {
+
+        context.pause(5)
+        if (node_.tryChangeBuffer(-100)) {
+          if (crushedSlot == null)
+            crushedSlot = recipes(ore).copy()
+          else if (crushedSlot.getItem() == recipes(ore).getItem())
+            crushedSlot.stackSize += 2
+          else
+            return Array(null, "Target stack full")
+          return Array(true.asInstanceOf[java.lang.Boolean])
+        }
+        return Array(null, "Not enough power")
       }
-      return Array(null, "Not enough power")
     }
+    return Array(null, "Not Crushable Ore.")
   }
+
   def doSendItem(stack: ItemStack, s: Int, destination: ItemDestination): Boolean = {
     if (destination.recieveItem(stack)) {
       this.setInventorySlotContents(s, null)
@@ -57,6 +62,7 @@ class TileEntityPressureCrusher extends TileEntityEnvironment with AddressPastab
     }
     return false
   }
+
   @Callback
   def sendItem(context: Context, arguments: Arguments): Array[AnyRef] = {
     context.pause(0.5)
@@ -73,18 +79,33 @@ class TileEntityPressureCrusher extends TileEntityEnvironment with AddressPastab
     }.asInstanceOf[java.lang.Boolean])
     Array(true.asInstanceOf[java.lang.Boolean])
   }
+
   def closeInventory(): Unit = {}
-  def decrStackSize(i: Int, amount: Int): net.minecraft.item.ItemStack = if (i == 0) { return crushedSlot.splitStack(amount) } else return crushingSlot.splitStack(amount)
+
+  def decrStackSize(i: Int, amount: Int): net.minecraft.item.ItemStack = if (i == 0) {
+    return crushedSlot.splitStack(amount)
+  } else return crushingSlot.splitStack(amount)
+
   def getInventoryName(): String = "Pressure Crusher"
+
   def getInventoryStackLimit(): Int = 64
+
   def getSizeInventory(): Int = 2
+
   def getStackInSlot(i: Int): net.minecraft.item.ItemStack = if (i == 0) return crushedSlot else return crushingSlot
+
   def getStackInSlotOnClosing(i: Int): net.minecraft.item.ItemStack = null
+
   def hasCustomInventoryName(): Boolean = false
+
   def isItemValidForSlot(i: Int, stack: net.minecraft.item.ItemStack): Boolean = if (i == 0) return false else return true
+
   def isUseableByPlayer(player: net.minecraft.entity.player.EntityPlayer): Boolean = true
+
   def openInventory(): Unit = {}
+
   def setInventorySlotContents(i: Int, stack: net.minecraft.item.ItemStack): Unit = if (i == 1) crushingSlot = stack else crushedSlot = stack
+
   def recieveItem(item: net.minecraft.item.ItemStack): Boolean =
     if (crushingSlot == null) {
       crushingSlot = item
@@ -93,6 +114,8 @@ class TileEntityPressureCrusher extends TileEntityEnvironment with AddressPastab
       crushingSlot.stackSize += item.stackSize
       return true
     } else return false
+
   def inventory: com.awesomesauce.minecraft.forge.core.lib.util.ReadOnlyInventory = new InventoryWrapper(this)
+
   def sendItem(slot: Int): Unit = if (slot == 0) crushedSlot = null else crushingSlot = null
 }

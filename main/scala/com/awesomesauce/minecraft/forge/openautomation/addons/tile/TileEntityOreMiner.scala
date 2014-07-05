3545cf5940d1b8a7d64fe9e74d6836bf50b5573b
gjgfuj <gjgfujb@gmail.com>
2014-07-06 09:28:48 +1000

    Added ore miner skeleton

---
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/addons/tile/TileEntityOreMiner.scala
@@ -0,0 +1,64 @@
+package com.awesomesauce.minecraft.forge.openautomation.addons.tile
+
+import com.awesomesauce.minecraft.forge.openautomation.api.ItemInput
+import com.awesomesauce.minecraft.forge.openautomation.api.tools.AddressPastable
+import li.cil.oc.api.Network
+import li.cil.oc.api.network.{Arguments, Callback, Context, Visibility}
+import li.cil.oc.api.prefab.TileEntityEnvironment
+
+
+class TileEntityOreMiner extends TileEntityEnvironment with AddressPastable with ItemInput {
+  val node_ = Network.newNode(this, Visibility.Network).withComponent("pressurecrusher").withConnector(1000).create()
+  node = node_
+  var address: String = "xxx"
+  var maxX: Int = 100
+  var maxY: Int = 100
+  var maxZ: Int = 100
+  var minX: Int = 100
+  var minY: Int = 100
+  var minZ: Int = 100
+
+  def pasteAddress(add: String) = address = add
+
+  @Callback
+  def setAddress(context: Context, arguments: Arguments): Array[AnyRef] = {
+    address = arguments.checkString(0)
+    Array(true.asInstanceOf[java.lang.Boolean])
+  }
+
+  @Callback
+  def getAddress(context: Context, arguments: Arguments): Array[AnyRef] = {
+    Array(address)
+  }
+
+  @Callback
+  def setMin(context: Context, arguments: Arguments): Array[AnyRef] = {
+    minX = arguments.checkInteger(0)
+    minY = arguments.checkInteger(1)
+    minZ = arguments.checkInteger(2)
+    Array(true.asInstanceOf[java.lang.Boolean])
+  }
+
+  @Callback
+  def getMin(context: Context, arguments: Arguments): Array[AnyRef] = {
+    Array(minX, minY, minZ)
+  }
+
+  @Callback
+  def setMax(context: Context, arguments: Arguments): Array[AnyRef] = {
+    maxX = arguments.checkInteger(0)
+    maxY = arguments.checkInteger(1)
+    maxZ = arguments.checkInteger(2)
+    Array(true.asInstanceOf[java.lang.Boolean])
+  }
+
+  @Callback
+  def getMax(context: Context, arguments: Arguments): Array[AnyRef] = {
+    Array(maxX, maxY, maxZ)
+  }
+
+  @Callback
+  def mine(context: Context, arguments: Arguments): Array[AnyRef] = {
+    #TODO
+  }
+}

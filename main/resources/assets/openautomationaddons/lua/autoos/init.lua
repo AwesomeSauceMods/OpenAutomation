d5d6cff81ce55c2b5da9e196e9f4fcd46694bbe9
gjgfuj <gjgfujb@gmail.com>
2014-08-13 07:19:36 +1000

    Driver System

--- main/resources/assets/openautomationaddons/lua/autoos/init.lua
+++ main/resources/assets/openautomationaddons/lua/autoos/init.lua
@@ -6,16 +6,12 @@ for c, t in component.list() do
 end
 os.sleep(0.5) -- Allow signal processing by libraries.
 computer.pushSignal("init") -- so libs know components are initialized.
-local supportedComponentTypes = {}
 io.write("AutoOS initialized.")
 io.write("Locating components.")
 local components = {}
 local i = 0
 for c, t in component.list() do
-    if table:hasValue(supportedComponentTypes, t) then
-        components[i] = component.proxy(c)
-    end
-end
-for index, component in pairs(components) do
-    
+    local driver = require(t)
+    components[i] = { driver, component.proxy(c) }
+    driver.register(components[i][1])
 end

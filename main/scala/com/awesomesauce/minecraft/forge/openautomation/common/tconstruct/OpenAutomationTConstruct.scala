6d7d3f6707853c545e374d891e5ed99c2785dc9d
gjgfuj <gjgfujb@gmail.com>
2014-11-15 14:48:15 +1100

    Added Recipe, texture, and name for the Micro-Smeltery

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/common/tconstruct/OpenAutomationTConstruct.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/common/tconstruct/OpenAutomationTConstruct.scala
@@ -3,12 +3,14 @@ package com.awesomesauce.minecraft.forge.openautomation.common.tconstruct
 import com.awesomesauce.minecraft.forge.core.lib.util.ItemUtil
 import com.awesomesauce.minecraft.forge.openautomation.common.OAModule
 import com.awesomesauce.minecraft.forge.openautomation.common.tconstruct.te.TileEntityMelter
+import cpw.mods.fml.common.registry.GameRegistry
 import net.minecraft.block.Block
 import net.minecraft.block.material.Material
+import net.minecraft.item.ItemStack
+import net.minecraftforge.oredict.ShapedOreRecipe
 
 object OpenAutomationTConstruct extends OAModule {
   var melter: Block = null
-  var compactSmeltery2: Block = null
 
   def preInit() = {
 
@@ -16,6 +18,12 @@ object OpenAutomationTConstruct extends OAModule {
 
   def init() = {
     melter = ItemUtil.makeBlock(oa, "tconstruct.melter", Material.rock, () => new TileEntityMelter)
+    val materials = GameRegistry.findItem("TConstruct", "materials")
+    val smeltery = GameRegistry.findBlock("TConstruct", "Smeltery")
+    ItemUtil.addRecipe(oa, new ShapedOreRecipe(new ItemStack(melter), "xyx", "awa", "xzx",
+      Character.valueOf('x'), new ItemStack(materials, 1, 2), Character.valueOf('y'), "ingotAwesomeite",
+      Character.valueOf('z'), "awesomeCore", Character.valueOf('w'), new ItemStack(smeltery, 1, 0),
+      Character.valueOf('a'), new ItemStack(smeltery, 1, 1)))
   }
 
   def postInit() = {

6aec8134cf6cae3514c34ec0f2e1f17750c5a5b5
gjgfuj <gjgfujb@gmail.com>
2014-12-06 21:43:05 +1100

    Added Player Lasers. - Part 7

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/common/lasers/packets/EntityPacket.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/common/lasers/packets/EntityPacket.scala
@@ -1,16 +1,16 @@
 package com.awesomesauce.minecraft.forge.openautomation.common.lasers.packets
 
 import com.awesomesauce.minecraft.forge.openautomation.api.lasers.LaserPacket
-import net.minecraft.entity.Entity
+import net.minecraft.entity.EntityLivingBase
 import net.minecraft.world.World
 import net.minecraftforge.common.util.ForgeDirection
 
-class EntityPacket(entity: Entity) extends LaserPacket {
+class EntityPacket(entity: EntityLivingBase) extends LaserPacket {
   def arrive(world: World, x: Int, y: Int, z: Int, to: ForgeDirection) = {
     if (!world.isAirBlock(x + to.offsetX, y + to.offsetY, z + to.offsetZ))
       false
     else {
-      entity.setPosition(x + to.offsetX, y + to.offsetY, z + to.offsetZ)
+      entity.setPositionAndUpdate(x + to.offsetX, y + to.offsetY, z + to.offsetZ)
       true
     }
   }

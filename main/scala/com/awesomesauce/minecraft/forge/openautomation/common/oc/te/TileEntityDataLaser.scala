b117a246ed96fff165ea45ea9d4e477d05fb3026
gjgfuj <gjgfujb@gmail.com>
2014-12-07 00:28:41 +1100

    Finalized the OC and Player Lasers. ish. No recipe yet.

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/common/oc/te/TileEntityDataLaser.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/common/oc/te/TileEntityDataLaser.scala
@@ -10,11 +10,11 @@ import li.cil.oc.api.prefab.TileEntityEnvironment
 import net.minecraftforge.common.util.ForgeDirection
 
 class TileEntityDataLaser extends TileEntityEnvironment with BasicDismantleableTile {
-  val node_ = Network.newNode(this, Visibility.Network).withComponent("dataLaser").withConnector(1000).create()
+  val node_ = Network.newNode(this, Visibility.Network).withComponent("laser").withConnector(1000).create()
   node = node_
 
   @Callback(doc = "function(side:integer,data:object):boolean -- Sends a data laser with said packet.")
-  def sendLaser(context: Context, arguments: Arguments): Array[AnyRef] = {
+  def sendDataLaser(context: Context, arguments: Arguments): Array[AnyRef] = {
     node_.tryChangeBuffer(-1)
     Array(LaserHelper.sendLaser(worldObj, xCoord, yCoord, zCoord,
       ForgeDirection.getOrientation(arguments.checkInteger(0)), new DataPacket(arguments.checkAny(1))).asInstanceOf[java.lang.Boolean])

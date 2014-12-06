8c353b76992f6b3c4bc9f0f2f692e894df936d6c
gjgfuj <gjgfujb@gmail.com>
2014-12-07 10:21:54 +1100

    Added ability to right click on laser emitter to see what's up. - part 2.

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/common/lasers/te/TileEntityEnergyLaserEmitter.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/common/lasers/te/TileEntityEnergyLaserEmitter.scala
@@ -10,22 +10,25 @@ import net.minecraft.tileentity.TileEntity
 import net.minecraftforge.common.util.ForgeDirection
 
 class TileEntityEnergyLaserEmitter extends TileEntity with IEnergyHandler with BasicDismantleableTile with TActivatedTileEntity {
-  var dunnit = false
+  var dunnit = 0
+  var maxDunnit = 0
 
   def activate(player: EntityPlayer, side: Int, partx: Float, party: Float, partz: Float): Boolean = {
-    PlayerUtil.sendChatMessage(player, dunnit.toString)
+    PlayerUtil.sendChatMessage(player, dunnit.toString + "RF sent last laser.")
+    PlayerUtil.sendChatMessage(player, maxDunnit.toString + "RF sent last laser.")
   }
   def extractEnergy(from: ForgeDirection, maxReceive: Int, simulate: Boolean) = 0
   def getEnergyStored(from: ForgeDirection) = 0
 
   def getMaxEnergyStored(from: ForgeDirection) = 10000
   def receiveEnergy(from: ForgeDirection, maxReceive: Int, simulate: Boolean) = {
+    maxDunnit = maxReceive
     if (LaserHelper.sendLaser(worldObj, xCoord, yCoord, zCoord, from.getOpposite, new EnergyPacket(maxReceive))) {
-      dunnit = true
+      dunnit = maxReceive
       maxReceive
     }
     else {
-      dunnit = false
+      dunnit = 0
       0
     }
   }

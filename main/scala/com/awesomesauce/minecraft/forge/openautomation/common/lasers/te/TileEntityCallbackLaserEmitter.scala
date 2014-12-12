651b2b0569ba735435b6038718d3d5f41b65fa57
gjgfuj <gjgfujb@gmail.com>
2014-12-12 20:35:53 +1100

    Get callbacks to work - Part 3.

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/common/lasers/te/TileEntityCallbackLaserEmitter.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/common/lasers/te/TileEntityCallbackLaserEmitter.scala
@@ -29,7 +29,7 @@ class TileEntityCallbackLaserEmitter extends TileEnergyReceiver with TActivatedT
     currentCallbackNum += 1
     currentCallback = LaserAPI.callbacks.get(currentCallbackNum)
     while (!currentCallback.isUseableOn(world, x, y, z, to)) {
-      if (currentCallbackNum >= LaserAPI.callbacks.size)
+      if (currentCallbackNum >= LaserAPI.callbacks.size - 1)
         currentCallbackNum = -1
       currentCallbackNum += 1
       currentCallback = LaserAPI.callbacks.get(currentCallbackNum)

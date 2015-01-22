54f7c637982f3e046027e12c9bc697a12de8cab3
Sandra <gjgfujb@gmail.com>
2015-01-22 13:14:41 +1100

    Fixed warning thing.

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/common/oc/oalang/ScriptPart.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/common/oc/oalang/ScriptPart.scala
@@ -5,12 +5,12 @@ class ScriptPart(line: String) {
   val splittedLine = line.split(" > ")
   val commandSplit: Array[String] = splittedLine(0).split(" ")
   val component = commandSplit(0)
-  if (splittedLine.length > 1) {
-    resultVars = splittedLine(1).split(" ")
-  }
   val callback = commandSplit(1)
   val arguments = commandSplit.drop(2)
   var resultVars = Array[String]("")
+  if (splittedLine.length > 1) {
+    resultVars = splittedLine(1).split(" ")
+  }
 
   override def toString = line
 

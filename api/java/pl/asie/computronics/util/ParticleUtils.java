16ccdccd8e390cbd3373eda6ffa6475d4b923e3d
Sandra <gjgfujb@gmail.com>
2015-01-22 15:45:49 +1100

    #totallynothacky method of doing particle effects using computronics because I'm a dirty cheater.

---
+++ api/java/pl/asie/computronics/util/ParticleUtils.java
@@ -0,0 +1,9 @@
+package pl.asie.computronics.util;
+
+import net.minecraft.world.World;
+
+public class ParticleUtils {
+	public static void sendParticlePacket(String name, World worldObj, double x, double y, double z, double vx, double vy, double vz) {
+		//EMPTY THING BECAUSE I'M A DIRTY CHEATER.
+    }
+}
--- main/scala/com/awesomesauce/minecraft/forge/openautomation/api/lasers/LaserHelper.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/api/lasers/LaserHelper.scala
@@ -1,9 +1,11 @@
 package com.awesomesauce.minecraft.forge.openautomation.api.lasers
 
+import cpw.mods.fml.common.Loader
 import net.minecraft.world.World
 import net.minecraftforge.common.util.ForgeDirection
 
 object LaserHelper {
+  val computronicsLoaded = Loader.isModLoaded("Computronics")
   def sendLaser(world: World, x: Int, y: Int, z: Int, dir: ForgeDirection, packet: LaserPacket): Boolean = {
     var nx = x + dir.offsetX
     var ny = y + dir.offsetY
@@ -14,6 +16,8 @@ object LaserHelper {
       nx += dir.offsetX
       ny += dir.offsetY
       nz += dir.offsetZ
+      if (computronicsLoaded)
+        pl.asie.computronics.util.ParticleUtils.sendParticlePacket("reddust", world, nx, ny, nz, 0,0,0)
     }
     if (world.getTileEntity(nx, ny, nz).isInstanceOf[LaserReciever]) {
       return world.getTileEntity(nx, ny, nz).asInstanceOf[LaserReciever].arrive(dir.getOpposite, packet)

4a11f997419074d89a41db1551cdc4d842982be6
Sandra <gjgfujb@gmail.com>
2015-01-24 16:11:38 +1100

    Made laser visibility at the start and not the end.

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/api/lasers/LaserHelper.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/api/lasers/LaserHelper.scala
@@ -12,12 +12,12 @@ object LaserHelper {
     var nz = z + dir.offsetZ
     var counter = 0
     while (!world.getBlock(nx, ny, nz).isOpaqueCube && counter < 400) {
+      if (computronicsLoaded)
+        pl.asie.computronics.util.ParticleUtils.sendParticlePacket(packet.particleEffect, world, nx + 0.5, ny + 0.5, nz + 0.5, 0, 0, 0)
       counter += 1
       nx += dir.offsetX
       ny += dir.offsetY
       nz += dir.offsetZ
-      if (computronicsLoaded)
-        pl.asie.computronics.util.ParticleUtils.sendParticlePacket(packet.particleEffect, world, nx+0.5, ny+0.5, nz+0.5, 0,0,0)
     }
     if (world.getTileEntity(nx, ny, nz).isInstanceOf[LaserReciever]) {
       return world.getTileEntity(nx, ny, nz).asInstanceOf[LaserReciever].arrive(dir.getOpposite, packet)

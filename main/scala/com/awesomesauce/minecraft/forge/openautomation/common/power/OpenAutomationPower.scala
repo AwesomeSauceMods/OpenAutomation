21545cc1c2fad247e670819661d0358cdc701d30
gjgfuj <gjgfujb@gmail.com>
2015-04-12 01:44:39 +1000

    Added recipes for energiser and energisable emitter. Have fun!

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/common/power/OpenAutomationPower.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/common/power/OpenAutomationPower.scala
@@ -2,10 +2,14 @@ package com.awesomesauce.minecraft.forge.openautomation.common.power
 
 import com.awesomesauce.minecraft.forge.core.lib.util.ItemUtil
 import com.awesomesauce.minecraft.forge.openautomation.common.OAModule
+import com.awesomesauce.minecraft.forge.openautomation.common.lasers.OpenAutomationLasers
 import com.awesomesauce.minecraft.forge.openautomation.common.power.te._
 import net.minecraft.block.Block
 import net.minecraft.block.material.Material
+import net.minecraft.init.Blocks
+import net.minecraft.item.ItemStack
 import net.minecraftforge.common.config.Configuration
+import net.minecraftforge.oredict.{ShapedOreRecipe, ShapelessOreRecipe}
 
 
 object OpenAutomationPower extends OAModule {
@@ -22,7 +26,10 @@ object OpenAutomationPower extends OAModule {
     fuelOutputModifier = oa.config.get(Configuration.CATEGORY_GENERAL, "power:fuelOutputModifier", 20, "Multiplier for fuel output in RF.").getInt
   }
 
-  def init() = {}
+  def init() = {
+    ItemUtil.addRecipe(oa, new ShapelessOreRecipe(new ItemStack(laserEnergiserEmitter), OpenAutomationLasers.energyLaserEmitter, Blocks.redstone_block, "ingotElectrum"))
+    ItemUtil.addRecipe(oa, new ShapedOreRecipe(new ItemStack(laserEnergiser), "xxx", "xyx", "yzy", 'x', "dustRedstone", 'y', "laserMirror", 'z', Blocks.furnace))
+  }
 
   def postInit() = {}
 }

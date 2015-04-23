7e3381422b3cce42f9f1cffd121186225d0ab4a2
Sandra <gjgfujb@gmail.com>
2015-04-23 21:53:02 +1000

    Upgraded to OC1.5 API.
    Added barebones bauble computers. (Unobtainable.)

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/common/oc/baubles/item/ItemMachineRing.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/common/oc/baubles/item/ItemMachineRing.scala
@@ -1,10 +1,14 @@
 package com.awesomesauce.minecraft.forge.openautomation.common.oc.baubles.item
 
+import java.util
+
 import baubles.api.{BaubleType, IBauble}
+import li.cil.oc.api.Driver
 import li.cil.oc.api.machine.MachineHost
-import li.cil.oc.api.network.Node
+import li.cil.oc.api.network.ManagedEnvironment
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.item.{Item, ItemStack}
+import net.minecraftforge.common.util.Constants
 
 
 class ItemMachineBauble(bType: BaubleType) extends Item with IBauble {
@@ -34,41 +38,45 @@ class ItemMachineBauble(bType: BaubleType) extends Item with IBauble {
   override def canUnequip(stack: ItemStack, player: EntityLivingBase) = true
 
   class MachineBaubleHost(stack: ItemStack, player: EntityLivingBase) extends MachineHost {
-    val machine = li.cil.oc.api.Machine.create(this)
+    lazy final val machine = li.cil.oc.api.Machine.create(this)
+    val nbt = stack.getTagCompound
+    val itemNBT = nbt.getTagList("items", Constants.NBT.TAG_COMPOUND)
+    private val inventory = new Array[ItemStack](itemNBT.tagCount())
+    private val components = new Array[ManagedEnvironment](inventory.length)
+    private val updatingComponents = new util.ArrayList[ManagedEnvironment](inventory.length)
+    init()
 
-    def maxComponents = 0
+    def init() = {
+      for (i <- Range(0, itemNBT.tagCount())) {
+        val in = i
+        // Inventory loading.
+        val stackNbt = itemNBT.getCompoundTagAt(in)
+        val stack = ItemStack.loadItemStackFromNBT(stackNbt)
+        inventory(in) = stack
+        val driver = Driver.driverFor(stack, getClass)
+        if (stack != null && driver != null) {
+          val environment = driver.createEnvironment(stack, this)
+          if (environment != null) {
+            environment.load(driver.dataTag(stack))
+          }
+          components(in) = environment
+          if (environment != null && environment.canUpdate) {
+            updatingComponents.add(environment)
+          }
+        }
+      }
+    }
 
-    //TODO
-    def markForSaving() = {}
+    def markChanged() = {}
 
-    //TODO
-    def callBudget = 0
-
-    //TODO
+    def internalComponents = inventory
     def world = player.worldObj
 
-    def installedMemory = 0
-
-    //TODO
     def xPosition = player.posX
 
     def yPosition = player.posY
 
     def zPosition = player.posZ
-
-    def markChanged() = {}
-
-    //TODO
-    def onMachineDisconnect(node: Node) = {}
-
-    //TODO
-    def onMachineConnect(node: Node) = {}
-
-    //TODO
-    def cpuArchitecture = null
-
-    //TODO
-    def componentSlot(address: String) = 0 //TODO
   }
 
 }

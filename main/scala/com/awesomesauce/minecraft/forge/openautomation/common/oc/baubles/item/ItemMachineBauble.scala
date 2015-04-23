6b28d41545be807c5d008b8e32544c4df63d614c
Sandra <gjgfujb@gmail.com>
2015-04-23 22:22:57 +1000

    Should Build Now.

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/common/oc/baubles/item/ItemMachineBauble.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/common/oc/baubles/item/ItemMachineBauble.scala
@@ -1,11 +1,12 @@
 package com.awesomesauce.minecraft.forge.openautomation.common.oc.baubles.item
 
 import java.util
+import java.util.Objects
 
 import baubles.api.{BaubleType, IBauble}
 import li.cil.oc.api.Driver
 import li.cil.oc.api.machine.MachineHost
-import li.cil.oc.api.network.ManagedEnvironment
+import li.cil.oc.api.network.{ManagedEnvironment, Node}
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.item.{Item, ItemStack}
 import net.minecraftforge.common.util.Constants
@@ -41,9 +42,9 @@ class ItemMachineBauble(bType: BaubleType) extends Item with IBauble {
     lazy final val machine = li.cil.oc.api.Machine.create(this)
     val nbt = stack.getTagCompound
     val itemNBT = nbt.getTagList("items", Constants.NBT.TAG_COMPOUND)
-    private val inventory = new Array[ItemStack](itemNBT.tagCount())
-    private val components = new Array[ManagedEnvironment](inventory.length)
-    private val updatingComponents = new util.ArrayList[ManagedEnvironment](inventory.length)
+    private val inventory = new util.ArrayList[ItemStack](itemNBT.tagCount())
+    private val components = new util.ArrayList[ManagedEnvironment](inventory.size)
+    private val updatingComponents = new util.ArrayList[ManagedEnvironment](inventory.size)
     init()
 
     def init() = {
@@ -52,14 +53,14 @@ class ItemMachineBauble(bType: BaubleType) extends Item with IBauble {
         // Inventory loading.
         val stackNbt = itemNBT.getCompoundTagAt(in)
         val stack = ItemStack.loadItemStackFromNBT(stackNbt)
-        inventory(in) = stack
+        inventory.set(in, stack)
         val driver = Driver.driverFor(stack, getClass)
         if (stack != null && driver != null) {
           val environment = driver.createEnvironment(stack, this)
           if (environment != null) {
             environment.load(driver.dataTag(stack))
           }
-          components(in) = environment
+          components.set(in, environment)
           if (environment != null && environment.canUpdate) {
             updatingComponents.add(environment)
           }
@@ -77,6 +78,20 @@ class ItemMachineBauble(bType: BaubleType) extends Item with IBauble {
     def yPosition = player.posY
 
     def zPosition = player.posZ
+
+    def onMachineDisconnect(node: Node) = {}
+
+    def onMachineConnect(node: Node) = {}
+
+    def componentSlot(address: String): Int = {
+      for (i <- Range(0, components.size())) {
+        val environment = components.get(i)
+        if (environment != null && environment.node() != null && Objects.equals(environment.node().address(), address)) {
+          return i
+        }
+      }
+      -1
+    }
   }
 
 }

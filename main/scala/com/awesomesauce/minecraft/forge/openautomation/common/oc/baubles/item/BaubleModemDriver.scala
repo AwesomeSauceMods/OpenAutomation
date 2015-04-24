f1ea5120aba25c3584f0d79241ece5d727f616db
Sandra <gjgfujb@gmail.com>
2015-04-24 19:57:26 +1000

    Removed modem driver temporarily.

--- main/scala/com/awesomesauce/minecraft/forge/openautomation/common/oc/baubles/item/BaubleModemDriver.scala
+++ main/scala/com/awesomesauce/minecraft/forge/openautomation/common/oc/baubles/item/BaubleModemDriver.scala
@@ -1,7 +1,7 @@
 package com.awesomesauce.minecraft.forge.openautomation.common.oc.baubles.item
 
-import li.cil.oc.api.driver
-
+/*
 object BaubleModemDriver extends driver.Item with driver.item.HostAware {
 
 }
+*/
